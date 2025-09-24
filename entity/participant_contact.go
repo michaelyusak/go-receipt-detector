406fa3a88941976555aa221da50ac3b70b309d6a
michaelyusak <michaelyusakt@gmail.com>
2025-09-24 18:49:03 +0700

    fix entities

--- entity/participant_contact.go
+++ entity/participant_contact.go
@@ -3,8 +3,8 @@ package entity
 type ParticipantContact struct {
 	ContactId     int64  `json:"contact_id"`
 	ParticipantId int64  `json:"participant_id"`
-	ContactType   string `json:"contact_type"`
-	ContactValue  string `json:"contact_value"`
+	ContactType   string `json:"contact_type" binding:"required"`
+	ContactValue  string `json:"contact_value" binding:"required"`
 	CreatedAt     int64  `json:"created_at"`
 	UpdatedAt     *int64 `json:"updated_at,omitempty"`
 	DeletedAt     *int64 `json:"deleted_at,omitempty"`
--- entity/receipt_participant.go
+++ entity/receipt_participant.go
@@ -5,12 +5,15 @@ import (
 )
 
 type ReceiptParticipant struct {
-	ParticipantId             int64            `json:"participant_id"`
-	ParticipantName           string           `json:"participant_name"`
-	ReceiptId                 int64            `json:"receipt_id"`
-	NoticeInterval            hEntity.Duration `json:"notice_interval"`
-	LastNotice                int64            `json:"last_notice"`
-	CreatedAt                 int64            `json:"created_at"`
-	UpdatedAt                 *int64           `json:"updated_at,omitempty"`
-	DeletedAt                 *int64           `json:"deleted_at,omitempty"`
+	ParticipantId   int64                `json:"participant_id"`
+	ParticipantName string               `json:"participant_name" binding:"required"`
+	ReceiptId       int64                `json:"receipt_id"`
+	Notifying       bool                 `json:"notifying"`
+	NoticeInterval  hEntity.Duration     `json:"notice_interval"`
+	LastNotice      *int64               `json:"last_notice"`
+	Contacts        []ParticipantContact `json:"contacts,omitempty" binding:"required,gte=1,dive"`
+	CreatedAt       int64                `json:"created_at"`
+	UpdatedAt       *int64               `json:"updated_at,omitempty"`
+	DeletedAt       *int64               `json:"deleted_at,omitempty"`
+}
 }
